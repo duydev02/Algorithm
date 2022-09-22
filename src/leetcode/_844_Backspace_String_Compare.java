4a3aa54e192da6e14dcfe85c288f722674ee35fd
duydev02 <minhduy12581@gmail.com>
2022-09-22 14:56:36 +0700

    More

---
+++ src/leetcode/_844_Backspace_String_Compare.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+public class _844_Backspace_String_Compare {
+//    public static boolean backspaceCompare(String S, String T) {
+//
+//    }
+
+    public static void main(String[] args) {
+        int number = 0;
+
+        // checks if number is greater than 0
+        if (number == 0) {
+            System.out.println("The number is positive.");
+        }
+
+        // checks if number is less than 0
+        else if (number < 2) {
+            System.out.println("The number is negative.");
+        }
+        else {
+            System.out.println("The number is 0.");
+        }
+    }
+}
